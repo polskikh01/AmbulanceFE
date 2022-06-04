97468cada9a52c268ca7e7853cfbb7092128702a
ChrisYuneva <christina_yuneva@mail.ru>
2022-06-04 22:25:38 +0400

    добавлена маршрутизация

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from "./map/map.component";
+import {MainComponent} from "./main/main.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main',  pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: 'map', component: MapComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
 import { CardItemComponent } from './card-item/card-item.component';
+import { MapComponent } from './map/map.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    CardItemComponent
+    CardItemComponent,
+    MapComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
